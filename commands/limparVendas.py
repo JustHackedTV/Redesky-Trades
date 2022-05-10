760e9ba681afb431c4b37e0c158ac39e7752e2bd
JustHackedTV <78915697+JustHackedTV@users.noreply.github.com>
2022-05-10 16:19:41 -0300

    Update para o sistema de procura

--- commands/limparVendas.py
+++ commands/limparVendas.py
@@ -7,6 +7,15 @@ def limparVendas(client, GUILDS):
     JFile = json.load(open('files/Listings.json'))
     if str(ctx.author.id) not in JFile or len(JFile[str(ctx.author.id)]) == 0:
       return await ctx.respond(":no_entry: Erro: `Você não possui nenhuma listing.`", ephemeral=True)
+    IJFile = json.load(open('files/Items.json'))
+    for listing in JFile[str(ctx.author.id)]:
+      if listing['item'] not in IJFile:
+        pass
+      else:
+        for i in IJFile[listing['item']]:
+          if i['dono'] == str(ctx.author.id):
+            IJFile[listing['item']].remove(i)
+    json.dump(IJFile, open('files/Items.json', 'w'))
     JFile[str(ctx.author.id)] = []
     json.dump(JFile, open('files/Listings.json', 'w'))
     await ctx.respond(":broom: Listings limpas com sucesso!")
