91fdf72f21c0e3632fa984a81cf173a31e6a95be
JustHackedTV <78915697+JustHackedTV@users.noreply.github.com>
2022-05-10 16:15:59 -0300

    Update para o sistema de procura
    vai com o arquivo "Items.json"

--- commands/createListing.py
+++ commands/createListing.py
@@ -13,9 +13,23 @@ def CreateListing(client, GUILDS):
     if quantidade < 0 or quantidade == None:
       return await ctx.respond("Erro: `Você não pode colocar um número negativo ou nulo de quantidade.`", ephemeral=True)
     JFile = json.load(open('files/Listings.json'))
+    IJfile = json.load(open('files/Items.json'))
+    if str(item) not in IJfile:
+      IJfile[str(item)] = []
     if not str(ctx.author.id) in JFile:
       JFile[str(ctx.author.id)] = []
+    IJfile[str(item)].append({'coins': coins, 'quantidade': quantidade, 'dono': str(ctx.author.id)})
     JFile[str(ctx.author.id)].append({'item': item, 'coins': coins, 'quantidade': quantidade})
     json.dump(JFile, open('files/Listings.json', 'w'))
-    embed = discord.Embed(title="Listing criada com sucesso!", description=f":pushpin: **Item**: `{item}`\n:moneybag: **Coins**: `{coins}`\n:receipt: **Quantidade**: `{quantidade}`", color=0x00ff00)
+    json.dump(IJfile, open('files/Items.json', 'w'))
+    coinSimplfied = 0
+    if coins >= 1000000000:
+      coinSimplfied = f"${int(coins / 1000000000)}b (bilhões)"
+    elif coins >= 1000000:
+      coinSimplfied = f"${int(coins / 1000000)}kk (milhões)"
+    elif coins >= 1000:
+      coinSimplfied = f"${int(coins / 1000)}k"
+    elif coins >= 1:
+      coinSimplfied = f"${int(coins)}"
+    embed = discord.Embed(title="Listing criada com sucesso!", description=f":pushpin: **Item**: `{item}`\n:moneybag: **Coins**: `{coinSimplfied}`\n:receipt: **Quantidade**: `{quantidade}`", color=0x00ff00)
     await ctx.respond(content=ctx.author.mention + " - :round_pushpin: Listing criada com sucesso!", embed=embed, ephemeral=True)
